691bcd8ba5ec1cbf3d8a252a13161e3f800168c0
cfraysse <cyrille.fraysse@gmail.com>
2025-07-04 19:03:34 +0200

    Code generation par boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const LORE_LEGACY = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+LORE_LEGACY.abilities = {
+  str: 'LORE_LEGACY.Ability.Str.long',
+  dex: 'LORE_LEGACY.Ability.Dex.long',
+  con: 'LORE_LEGACY.Ability.Con.long',
+  int: 'LORE_LEGACY.Ability.Int.long',
+  wis: 'LORE_LEGACY.Ability.Wis.long',
+  cha: 'LORE_LEGACY.Ability.Cha.long',
+};
+
+LORE_LEGACY.abilityAbbreviations = {
+  str: 'LORE_LEGACY.Ability.Str.abbr',
+  dex: 'LORE_LEGACY.Ability.Dex.abbr',
+  con: 'LORE_LEGACY.Ability.Con.abbr',
+  int: 'LORE_LEGACY.Ability.Int.abbr',
+  wis: 'LORE_LEGACY.Ability.Wis.abbr',
+  cha: 'LORE_LEGACY.Ability.Cha.abbr',
+};
