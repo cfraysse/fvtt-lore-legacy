6378561e3bb2e5388c0c0bd5c682bbd6ac7856bf
cfraysse <cyrille.fraysse@gmail.com>
2025-11-13 15:59:24 +0100

    Glisser une arme dans la barre de raccourcis crée une macro de lancer de dés de dégats

--- module/fvtt-lore-legacy.mjs
+++ module/fvtt-lore-legacy.mjs
@@ -117,7 +117,11 @@ Handlebars.registerHelper('toLowerCase', function (str) {
 
 Hooks.once('ready', function () {
   // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-  Hooks.on('hotbarDrop', (bar, data, slot) => createItemMacro(data, slot));
+  Hooks.on('hotbarDrop', async (bar, data, slot) => {
+    if (data.type !== 'Item') return true;
+    createItemMacro(data, slot);
+    return false;
+  });
 });
 
 /* -------------------------------------------- */
@@ -131,33 +135,35 @@ Hooks.once('ready', function () {
  * @param {number} slot     The hotbar slot to use
  * @returns {Promise}
  */
-async function createItemMacro(data, slot) {
+function createItemMacro(data, slot) {
   // First, determine if this is a valid owned item.
-  if (data.type !== 'Item') return;
   if (!data.uuid.includes('Actor.') && !data.uuid.includes('Token.')) {
     return ui.notifications.warn(
       'You can only create macro buttons for owned Items'
     );
   }
   // If it is, retrieve it based on the uuid.
-  const item = await Item.fromDropData(data);
-
-  // Create the macro command using the uuid.
+  Item.fromDropData(data).then((item) => {
   const command = `game.fvttlorelegacy.rollItemMacro("${data.uuid}");`;
-  let macro = game.macros.find(
-    (m) => m.name === item.name && m.command === command
-  );
-  if (!macro) {
-    macro = await Macro.create({
-      name: item.name,
-      type: 'script',
-      img: item.img,
-      command: command,
-      flags: { 'fvtt-lore-legacy.itemMacro': true },
-    });
-  }
-  game.user.assignHotbarMacro(macro, slot);
-  return false;
+    let macro = game.macros.find(
+      (m) => m.name === item.name && m.command === command
+    );
+    if (!macro) {
+      macro = Macro.create({
+        name: item.name,
+        type: 'script',
+        img: item.img,
+        command: command,
+        flags: { 'fvtt-lore-legacy.itemMacro': true },
+      }).then((macro_) =>{
+        game.user.assignHotbarMacro(macro_, slot);
+     });
+    }
+    else 
+    {
+      game.user.assignHotbarMacro(macro, slot);
+    }    
+  });
 }
 
 /**
